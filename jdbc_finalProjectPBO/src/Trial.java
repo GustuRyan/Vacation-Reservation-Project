c1226fc86fbd49c6a19d4075d9988b0ae90e261e
gus ryan <rian.putra2003@gmail.com>
2023-11-22 00:24:21 +0800

    Add GUI for Login Page

--- jdbc_finalProjectPBO/src/Trial.java
+++ jdbc_finalProjectPBO/src/Trial.java
@@ -1,53 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Trial {
-    public static void main(String[] args) {
-        // Memuat driver MySQL
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Gagal memuat driver MySQL.");
-            e.printStackTrace();
-            return;
-        }
 
-        String jdbcUrl = "jdbc:mysql://localhost:127.0.0.1/classicmodels";
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://127.0.0.1:3306/classicmodels";
         String username = "root";
         String password = "Root.127gusryan";
 
-        // ...
         try {
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            System.out.println("Koneksi ke MySQL berhasil!");
+            // Memastikan driver terdaftar
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Membuat pernyataan SQL INSERT
-            String insertQuery = "INSERT INTO customers (customerNumber, customerName, phone) VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            // Menghubungkan ke database
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
 
-            // Mengisi nilai parameter pada pernyataan
-            preparedStatement.setString(1, "NilaiKolom1");
-            preparedStatement.setString(2, "NilaiKolom2");
-            preparedStatement.setString(3, "NilaiKolom3");
+            if (connection != null) {
+                System.out.println("Koneksi ke database berhasil!");
+                // Lakukan operasi database di sini
 
-            // Menjalankan pernyataan INSERT
-            int rowsAffected = preparedStatement.executeUpdate();
-
-            if (rowsAffected > 0) {
-                System.out.println("Data berhasil dimasukkan ke database.");
-            } else {
-                System.out.println("Gagal memasukkan data ke database.");
+                // Tutup koneksi setelah selesai
+                connection.close();
             }
 
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Koneksi ke database gagal!");
             e.printStackTrace();
-            System.err.println("Gagal terhubung ke MySQL.");
-        } finally {
-            // Pastikan untuk menutup koneksi saat selesai
-            // ...
         }
-
     }
 }
